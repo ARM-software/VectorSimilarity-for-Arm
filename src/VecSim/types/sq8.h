6dbc6018af2da8b041ef3adfd0452406a4a554b0
Zhang Xiangze <Xiangze.Zhang@arm.com>
2026-05-08 09:45:33 +0000

    Support norm in QuantPreprocessor

--- src/VecSim/types/sq8.h
+++ src/VecSim/types/sq8.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2006-Present, Redis Ltd.
  * All rights reserved.
+ * SPDX-FileCopyrightText: Copyright 2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
  *
  * Licensed under your choice of the Redis Source Available License 2.0
  * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
@@ -39,6 +40,28 @@ struct sq8 {
 
     template <VecSimMetric Metric>
     static constexpr size_t query_metadata_count() {
+        return (Metric == VecSimMetric_L2) ? 2 : 1;
+    }
+
+    // WithNorm variants: one extra metadata slot for x_mean_ip / y_mean_ip
+    template <VecSimMetric Metric>
+    static constexpr size_t storage_metadata_count_with_norm() {
+        return (Metric == VecSimMetric_L2) ? 5 : 4;
+    }
+
+    template <VecSimMetric Metric>
+    static constexpr size_t query_metadata_count_with_norm() {
+        return (Metric == VecSimMetric_L2) ? 3 : 2;
+    }
+
+    // Index of x_mean_ip / y_mean_ip in the metadata array (after quantized values)
+    template <VecSimMetric Metric>
+    static constexpr size_t mean_ip_index() {
+        return (Metric == VecSimMetric_L2) ? 4 : 3;
+    }
+
+    template <VecSimMetric Metric>
+    static constexpr size_t query_mean_ip_index() {
         return (Metric == VecSimMetric_L2) ? 2 : 1;
     }
 };
